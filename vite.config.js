d94989feccd0f43bc262a80563ece9caa169127f
wang <wang1144301122@gmail.com>
2023-02-16 11:25:47 +0800

    ✨ feat(vite.config): 新增docs

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,20 @@ export default defineConfig({
             resolvers: [ElementPlusResolver()],
         }),
     ],
+    build:{
+        outDir: 'docs',
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/js/0-[name]-[hash].js',
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                assetFileNames: 'assets/[ext]/[name]-[hash][extname]',
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    },
 })
