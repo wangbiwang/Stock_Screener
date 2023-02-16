3a8ec7492b6209e573e3cff3ce36c03bd577597c
wang <wang1144301122@gmail.com>
2023-02-16 17:31:13 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,30 +4,39 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ],
-    build:{
-        outDir: 'docs',
-        chunkSizeWarningLimit: 1500,
-        rollupOptions: {
-            output: {
-                entryFileNames: 'assets/js/0-[name]-[hash].js',
-                chunkFileNames: 'assets/js/[name]-[hash].js',
-                assetFileNames: 'assets/[ext]/[name]-[hash][extname]',
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
-    },
+export default defineConfig(({ command, mode }) => {
+    let base
+    if (command === 'serve') {
+        base = './'
+    } else {
+        base = '/Stock_Screener/'
+    }
+    return {
+        base,
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
+        build: {
+            outDir: 'docs',
+            chunkSizeWarningLimit: 1500,
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'assets/js/0-[name]-[hash].js',
+                    chunkFileNames: 'assets/js/[name]-[hash].js',
+                    assetFileNames: 'assets/[ext]/[name]-[hash][extname]',
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                        }
+                    },
+                },
+            },
+        },
+    }
 })
